7d4c83badc1a758a059a38ec838e57c80a43772a
Henry <HWolf.binz@gmail.com>
2026-03-05 14:57:09 +0100

    feat(app): add grid layout for sidebar and main-content

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 import './style.scss';
+
+const App = () => {
+  const main = document.getElementById('app');
+
+  main!.innerHTML = `
+   <aside class="sidebar">
+      <div class="sidebar__header">NotesList</div>
+      <nav class="sidebar__nav">
+        <button id="nav-today">Heute</button>
+        <button id="nav-inbox">Eingang</button>
+      </nav>
+      <div class="sidebar__projects">
+        <h3>Projekte</h3>
+        <ul id="project-list"></ul>
+      </div>
+    </aside>
+    <section id="main-content">
+    </section>
+  `;
+};
+
+App();
